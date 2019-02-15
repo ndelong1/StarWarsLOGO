e7eeb36118330f81fb0d582cd7f65d52e5e4038e
ndelong1 <36314338+ndelong1@users.noreply.github.com>
2019-02-14 20:30:19 -0500

    Add files via upload

---
+++ Coding_Challenge/Code_A/sketch.js
@@ -0,0 +1,16 @@
+var tot=72;
+function setup() {
+	createCanvas(800, 800);
+}
+
+function draw() {
+	background(0);
+	translate (width/2,  height/2);
+	x=0;
+	y = x * 2;
+
+	point(for (var i = 0; i < tot; i++));
+	
+	
+	
+}
